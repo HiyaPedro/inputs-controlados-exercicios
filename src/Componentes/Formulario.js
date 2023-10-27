c9aa79748c421bea74bbe796b1b4f71067ac66e4
Peter <peterjcooper4@gmail.com>
2023-10-26 21:00:00 -0300

    exercicios-de-fixacao-inputs-controlados-feitos

--- src/Componentes/Formulario.js
+++ src/Componentes/Formulario.js
@@ -1,1 +1,38 @@
 //declare todo o componente formulário e seus inputs. Exporte o formulário.
+import React from 'react';
+
+
+function Formulário({nome,changeNome, pagamento,changePagamento, numero,changeNumero, validade,changeValidade, cvc,changeCvc, funcao}) {
+
+// const{nome,changeNome}=props
+
+    return (
+        
+            <form>
+                
+                <label>
+                    Nome da pessoa:
+                    <input value={nome} onChange={changeNome} />
+                </label>
+                <label>
+                    Forma de pagamento:
+                    <input value={pagamento} onChange={changePagamento} />
+                </label>
+                <label>
+                    Número do cartão:
+                    <input value={numero} onChange={changeNumero} />
+                </label>
+                <label>
+                    Data de validade:
+                    <input value={validade} onChange={changeValidade} />
+                </label>
+                <label>
+                    CVC:
+                    <input value={cvc} onChange={changeCvc} />
+                </label>
+                <button onClick={funcao}>Enviar dados</button>
+            </form>
+
+    )
+}
+export default Formulário
--- src/Pagina/PaginaDoCartao.js
+++ src/Pagina/PaginaDoCartao.js
@@ -1,27 +1,71 @@
 import Cartao from "../Componentes/Cartao";
 import { LayoutDaPagina } from "./estiloDaPaginaDoCartao";
-
-
-
+import Formulário from "../Componentes/Formulario";
+import React, {useState} from 'react'
 
 function PaginaDoCartao() {
     //declare os estados aqui
-
+    const[nome, setNome]=useState("")
+    const[pagamento, setPagamento]=useState("")
+    const[numero, setNumero]=useState("")
+    const[validade, setValidade]=useState("")
+    const[cvc, setCvc]=useState("")
 
     //declare as funções de controle de inputs aqui
+    const changeNome=(event)=>{
+        setNome(event.target.value)
+      }
+    const changePagamento=(event)=>{
+        setPagamento(event.target.value)
+      }
+    const changeNumero=(event)=>{
+        setNumero(event.target.value)
+      }
+    const changeValidade=(event)=>{
+        setValidade(event.target.value)
+      }
+    const changeCvc=(event)=>{
+        setCvc(event.target.value)
+      }
+
+      const limparInputs=()=>{
+        alert("Seu cartão foi cadastrado com sucesso!")
+        setPagamento("")
+        setNumero("")
+        setNome("")
+        setCvc("")
+        setValidade("Nenhum")
+    
+      }
 
     return (
         <LayoutDaPagina>
             {/* Passe as variáveis de estado para o Cartão. Use Props. */}
             <Cartao
-                pagamento={"Crédito"}
-                numero={"3652 6589 7458 1254"}
-                nome={"Luciano Naganawa"}
-                cvc={"131"}
-                validade={"03/29"}
+                pagamento={pagamento}
+                numero={numero}
+                nome={nome}
+                cvc={cvc}
+                validade={validade}
             />
 
             {/* Chame o Componente Formulário Aqui */}
+            <Formulário 
+            
+                pagamento={pagamento}
+                numero={numero}
+                nome={nome}
+                cvc={cvc}
+                validade={validade}
+
+                changePagamento={changePagamento}
+                changeNumero={changeNumero}
+                changeNome={changeNome}
+                changeCvc={changeCvc}
+                changeValidade={changeValidade}
+
+                funcao={limparInputs}
+                />
             {/* Passe a função de controle de input e variável de estado para o formulário. Para isso use Props */}
 
 
